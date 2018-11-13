5172c3f2d09eb17bdfbd9b789278e64292e70415
Knut Hühne <knut.huehne@futurice.com>
2018-11-13 21:37:01 +0100

    Allow picking tree genus to be displayed

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import mapboxgl from 'mapbox-gl';
-import "mapbox-gl/dist/mapbox-gl.css";
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 mapboxgl.accessToken = process.env.MAPBOX_TOKEN;
 
@@ -7,34 +7,70 @@ const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v9',
   center: [13.38, 52.49],
-  zoom: 11
+  zoom: 11,
 });
 
+const counter = document.createElement('div');
+counter.style.position = 'absolute';
+counter.style.top = 0;
+counter.style.left = 0;
+counter.style.padding = '10px';
+counter.style.backgrounrd = 'white';
+document.body.appendChild(counter);
 
-function loadAndAdd(url, count) {
+function loadAndAdd(url, collectedTrees) {
   fetch(url)
     .then(resp => resp.json())
-    .then((trees) => {
-      console.log(trees.count);
-      if (trees.count > 5000){
-        console.warn("Too many trees, aborting load");
-        return
+    .then(trees => {
+      if (trees.count > 5000) {
+        console.warn('Too many trees, aborting load');
+        return;
       }
-      map.addLayer({
-          "id": `trees-${count}`,
-          "type": "circle",
+      const nextTrees = collectedTrees
+        ? {
+            ...collectedTrees,
+            features: [...collectedTrees.features, ...trees.features],
+          }
+        : trees;
+      if (trees.next) {
+        loadAndAdd(trees.next, nextTrees);
+      } else {
+        counter.innerText = nextTrees.features.length;
+        map.addLayer({
+          id: 'trees',
+          type: 'circle',
           source: {
             type: 'geojson',
-            data: trees,
-          }
-        }
-      );
-      if (trees.next){
-        console.log(trees.next);
-        loadAndAdd(trees.next, Math.random());
+            data: nextTrees,
+          },
+        });
       }
-
-    })
+    });
 }
 
-loadAndAdd('https://trees.codefor.de/api/trees/?page_size=1000&district_id=Neukölln&genus_latin=POPULUS', 0);
+const addGenusFilter = () => {
+  const input = document.createElement('select');
+  const options = ['BUXUS', 'CALOCEDRUS', 'CARAGANA', 'Prunus'].map(tree => {
+    const option = document.createElement('option');
+    option.innerText = tree;
+    option.value = tree;
+    return option;
+  });
+
+  options.forEach(option => input.appendChild(option));
+  input.style.position = 'absolute';
+  input.style.top = 0;
+  input.style.right = 0;
+  document.body.appendChild(input);
+  input.onchange = event => filterGenus(event.target.value);
+};
+
+addGenusFilter();
+
+const filterGenus = name => {
+  if (map.getLayer('trees')) map.removeLayer('trees');
+  if (map.getSource('trees')) map.removeSource('trees');
+  loadAndAdd(
+    `https://trees.codefor.de/api/v2/trees/?page_size=1000&genus=${name}`
+  );
+};
